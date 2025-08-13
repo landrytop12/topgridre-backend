374967d46755b256d0578d713ae7d5fcabe5407f
landrytop12 <landry.monye@topafricans.com>
2025-08-13 21:56:51 +0100

    Delete topgridre-backend directory

--- topgridre-backend/src/middleware/adminAuth.js
+++
@@ -1,7 +0,0 @@
-export default function adminAuth(req, res, next) {
-  const key = req.header('x-admin-key');
-  if (!key || key !== process.env.ADMIN_API_KEY) {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-  next();
-}
